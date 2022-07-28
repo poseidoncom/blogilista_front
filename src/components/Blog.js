3937e11cd7938af2ee2448fb928de8eab6371b72
Tuukka <hartikainentuukka@gmail.com>
2022-07-28 14:43:36 +0300

    added unit tests

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,39 +3,34 @@ import { useState } from 'react'
 const Blog = ({ blog, like, remove, user }) => {
 
   const [visible, setVisible] = useState(false)
-  console.log(blog)
-  console.log(user)
-  if(visible){
-    return(
-      <div className="blog">
-        <table>
-          <tbody>
-            <tr><td><h3>{blog.title}</h3></td></tr>
-            <tr><td>{blog.url}</td></tr>
-            <tr><td>likes: {blog.likes}</td><td><button onClick={like}>like</button></td></tr>
-            <tr><td>{blog.author}</td></tr>
-            <tr><td>added by: {blog.user.name}</td></tr>
-            {user.username === blog.user.username ?
-              <tr><td><button onClick={remove}>remove</button></td></tr>:null }
-            <tr><td><button onClick={() => setVisible(false)}>hide</button></td></tr>
-          </tbody>
-        </table>
-      </div>
-    )
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
   }
 
   return(
     <div className="blog">
       <table>
         <tbody>
-          <tr><td><h3>{blog.title}</h3></td></tr>
-          <tr><td>{blog.url}</td></tr>
-          <tr><td><button onClick={() => setVisible(true)}>show</button></td></tr>
+          <div>
+            <tr><td><h3>{blog.title}</h3></td></tr>
+            <tr><td>{blog.author}</td></tr>
+            <tr><td><button onClick={toggleVisibility}>{visible ? 'hide': 'show'}</button></td></tr>
+          </div>
+          <div style={showWhenVisible} className="hidedContent">
+            <tr><td>likes: {blog.likes}</td><td><button onClick={like}>like</button></td></tr>
+            <tr><td>{blog.url}</td></tr>
+            <tr><td>added by: {blog.user.name}</td></tr>
+            {user.username === blog.user.username ?
+              <tr><td><button onClick={remove}>remove</button></td></tr>:null }
+          </div>
         </tbody>
       </table>
     </div>
-
   )
+
 
 }
 export default Blog
--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog.js'
 
-test('renders Author and Title but nothing else', () => {
+test('renders Title and url but nothing else', () => {
   const blog = {
     title: 'Testi',
     author: 'Mallikas Mikko',
@@ -13,6 +13,9 @@ test('renders Author and Title but nothing else', () => {
   render(<Blog blog={blog}/>)
   screen.debug()
 
-  const element = screen.getByText('Testi')
-  expect(element).toBeDefined()
+  const div = screen.querySelector('.togglableContent')
+  expect(div).toHaveStyle('display: none')
+
+
+
 })
